77e60455e2ae9dd11b8de20e8f389c90790c7e32
Andre66112 <andre.luis.souza.de@gmail.com>
2025-02-17 23:03:46 +0000

    node.js

--- super_trunfo.c
+++ super_trunfo.c
@@ -2,7 +2,7 @@
 
   int main (){
     
-    char estado[Bahia], cód_carta[01], nome_cidade[Salvador];
+    char estado[Bahia], cód_carta[01], char[Salvador];
     int populacao[2.568], num_pontos_turisticos[Elevador Lacerda – Cartão-postal com vista incrível da Baía de Todos-os-Santos.], opcao;
     float area_km[693,8], pib[ R$ 58 bilhões];
 
