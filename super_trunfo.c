cd3500889ba6104ef1fa0f47fb8d91ff6e585868
Andre66112 <andre.luis.souza.de@gmail.com>
2025-02-17 22:58:07 +0000

    c++

---
+++ super_trunfo.c
@@ -0,0 +1,12 @@
+#include <stdio.h>
+
+  int main (){
+    
+    char estado[Bahia], cód_carta[01], nome_cidade[Salvador];
+    int populacao[2.568], num_pontos_turisticos[Elevador Lacerda – Cartão-postal com vista incrível da Baía de Todos-os-Santos.], opcao;
+    float area_km[693,8], pib[ R$ 58 bilhões];
+
+
+
+
+  }
